81c45498eca7fb77e467db96f0e431a551527de4
qwertyyb <qwertyyb@qq.com>
2019-05-26 17:00:03 +0800

    调整通知

--- static/sw.js
+++ static/sw.js
@@ -4,22 +4,16 @@ self.addEventListener('push', e => {
 
   const { title, options } = e.data.json()
   const notification = self.registration.showNotification(title, { ...options })
-
-  console.log(e.waitUntil(notification))
-
 })
 
 self.addEventListener('notificationclick', function(event) {
   console.log('[Service Worker] Notification click Received.', event);
   const notification = event.notification
   console.log(notification)
-  const { data: { detailUrl, indexUrl } = {} } = notification
-  const url = event.action === 'showDetail' ? detailUrl : indexUrl
-  if (url) {
-    event.waitUntil(
-      clients.openWindow(url)
-    )
-  } else {
-    event.notification.close();
-  }
+  const { data: { url } = {} } = notification
+  url && event.waitUntil(
+    clients.openWindow(url)
+  )
+    
+  event.notification.close();
 });
